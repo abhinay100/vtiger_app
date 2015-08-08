d0bab8f59311d3beb9346422dbc791523d364bf7
abhinay100 <abhinay212@gmail.com>
2015-08-08 02:28:26 -0700

    database upload

--- config.db.php
+++ config.db.php
@@ -7,12 +7,12 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ********************************************************************************/
-  $dbconfig['db_server'] = '_DBC_SERVER_';
+  $dbconfig['db_server'] = 'localhost';
   $dbconfig['db_port'] = '_DBC_PORT_';
   $dbconfig['db_sockpath'] = '_DBC_SOCKPATH_';
-  $dbconfig['db_username'] = '_DBC_USER_';
-  $dbconfig['db_password'] = '_DBC_PASS_';
-  $dbconfig['db_name'] = '_DBC_NAME_';
+  $dbconfig['db_username'] = 'root';
+  $dbconfig['db_password'] = 'password';
+  $dbconfig['db_name'] = 'vtiger';
   $dbconfig['db_type'] = '_DBC_TYPE_';
   $dbconfig['db_bundled'] = '_DBC_BUNDLED_';
   $vtconfig['adminPwd'] = '_ADMIN_PASS_';
